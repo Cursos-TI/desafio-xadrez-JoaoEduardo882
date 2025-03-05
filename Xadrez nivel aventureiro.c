c241a8d80453e147f295cda8a7ed0c7fb1342e46
JoaoEduardo882 <jooaoeeduardo@gmail.com>
2025-03-05 21:57:05 +0000

    teste do codigo

--- Xadrez nivel aventureiro.c
+++ Xadrez nivel aventureiro.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Movimentação das Peças no Tabuleiro:\n");
     // Movimentação do Cavalo
-    printf("Movendo Cavalo: 2 casa para baixo e esquerda\n");
+    printf("Movendo Cavalo:\n");
     for (int i = 0; i < 2; i++) {
         printf("Baixo\n");  // Movendo para baixo
         for (int j = 0; j < 1; j++) {
